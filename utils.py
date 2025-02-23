7b5dcf40bc72bdf057ce08844a71d5e3d9f0fa1a
Keerthana811 <hkeerthi575@gmail.com>
2025-02-23 11:20:50 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,49 +1,49 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV, KFold
-from sklearn.ensemble import RandomForestRegressor, StackingRegressor
-from sklearn.linear_model import LinearRegression
-from xgboost import XGBRegressor
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-import joblib
+# Copyright Jonathan Hartley 2013. BSD 3-Clause license, see LICENSE file.
+from contextlib import contextmanager
+from io import StringIO
+import sys
+import os
 
 
+class StreamTTY(StringIO):
+    def isatty(self):
+        return True
 
-def load_data(filepath):
-    return pd.read_csv(filepath)
+class StreamNonTTY(StringIO):
+    def isatty(self):
+        return False
 
-def get_summary(data):
-    return {
-        "total_sales": data["Total"].sum(),
-        "avg_rating": data["Rating"].mean(),
-        "num_transactions": data.shape[0],
-    }
+@contextmanager
+def osname(name):
+    orig = os.name
+    os.name = name
+    yield
+    os.name = orig
 
-def train_models(data):
-    features = data[["Quantity", "Unit price"]]
-    target = data["Total"]
-    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+@contextmanager
+def replace_by(stream):
+    orig_stdout = sys.stdout
+    orig_stderr = sys.stderr
+    sys.stdout = stream
+    sys.stderr = stream
+    yield
+    sys.stdout = orig_stdout
+    sys.stderr = orig_stderr
 
-    rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
-    xgb_model = XGBRegressor(objective='reg:squarederror', n_estimators=100, random_state=42)
-    base_models = [("rf", rf_model), ("xgb", xgb_model)]
-    stack_model = StackingRegressor(estimators=base_models, final_estimator=LinearRegression())
+@contextmanager
+def replace_original_by(stream):
+    orig_stdout = sys.__stdout__
+    orig_stderr = sys.__stderr__
+    sys.__stdout__ = stream
+    sys.__stderr__ = stream
+    yield
+    sys.__stdout__ = orig_stdout
+    sys.__stderr__ = orig_stderr
 
-    stack_model.fit(X_train, y_train)
-    joblib.dump(stack_model, "../models/stacking_model.pkl" )
-    return stack_model
-
-def evaluate_model(model, X_test, y_test):
-    predictions = model.predict(X_test)
-    mae = mean_absolute_error(y_test, predictions)
-    mse = mean_squared_error(y_test, predictions)
-    r2 = r2_score(y_test, predictions)
-    return mae, mse, r2
-
-
-
-def predict_total_sales(model, input_data):
-    df_input = pd.DataFrame([input_data])
-    prediction = model.predict(df_input)[0]
-    return round(prediction, 2)
-
-
+@contextmanager
+def pycharm():
+    os.environ["PYCHARM_HOSTED"] = "1"
+    non_tty = StreamNonTTY()
+    with replace_by(non_tty), replace_original_by(non_tty):
+        yield
+    del os.environ["PYCHARM_HOSTED"]
